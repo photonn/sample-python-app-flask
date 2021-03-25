413f63a9f659fb781fbb8eb78a308c8fcfa62750
Miguel Martin <miguel_martin@epam.com>
2021-03-25 21:45:15 +0100

    upload template

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 myapp = Flask(__name__)
 
 @myapp.route("/")
 def hello():
-    return render_template('index.html')
+    return render_template('index.html', title='feo', css_location='static/style.css')
  
